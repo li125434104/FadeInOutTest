78cfa7664108f52fd8e44280c26c1de2780a6b42
li125434104 <125434104@qq.com>
2015-08-21 14:43:11 +0800

    Initial Commit

---
+++ FadeInOutTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FadeInOutTest
+//
+//  Created by LXJ on 15/8/21.
+//  Copyright (c) 2015年 GOME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FadeInOutTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FadeInOutTest
+//
+//  Created by LXJ on 15/8/21.
+//  Copyright (c) 2015年 GOME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
